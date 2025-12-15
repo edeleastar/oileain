af95bf60c698ae471fb2ce32202b3ddda5dea4f0
Eamonn de Leastar <edeleastar@wit.ie>
2025-12-15 07:32:33 +0000

    include appbar in base layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import "./layout.css";
   import favicon from "$lib/assets/favicon.svg";
-
+  import AppShell from "$lib/ui/appbar/AppShell.svelte";
   let { children } = $props();
 </script>
 
@@ -9,4 +9,10 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<AppShell>
+  <div
+    class="bg-surface-100-800-token border-surface-200-700-token mx-2 mb-2 w-auto overflow-hidden rounded-xl border-[1px] p-4 sm:mx-4 dark:bg-surface-950"
+  >
+    {@render children()}
+  </div>
+</AppShell>
