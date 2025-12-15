4a49806401877768f0acd7217d4dadf263c1f6d8
Eamonn de Leastar <edeleastar@wit.ie>
2025-12-15 08:56:33 +0000

    menui component

--- src/lib/ui/components/Icon.svelte
+++ src/lib/ui/components/Icon.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import Icon from "@iconify/svelte";
   import { Portal, Tooltip } from "@skeletonlabs/skeleton-svelte";
-  import { getIconColour, getIconType } from "./icons";
+  import { getIconColour, getIconType } from "../themes/icons";
 
   interface Props {
     type?: string;
