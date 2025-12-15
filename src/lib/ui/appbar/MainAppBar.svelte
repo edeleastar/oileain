d37149931c2ba396e078eaa8c670352c4fb87a4c
Eamonn de Leastar <edeleastar@wit.ie>
2025-12-15 07:32:19 +0000

    initial application bar

---
+++ src/lib/ui/appbar/MainAppBar.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { AppBar } from "@skeletonlabs/skeleton-svelte";
+  import { currentIsland, currentView } from "$lib/runes.svelte";
+  import Icon from "../components/Icon.svelte";
+  import Separator from "../components/Separator.svelte";
+</script>
+
+<AppBar>
+  <AppBar.Toolbar class="grid-cols-[auto_1fr_auto]">
+    <AppBar.Lead>
+      <div class="flex items-center gap-6">
+        <Separator />
+        <Icon type="oileain" height="80"></Icon>
+      </div>
+    </AppBar.Lead>
+    <AppBar.Headline>
+      <h1>Oileain: islands of Ireland : {currentView.value}</h1>
+      {#if currentIsland.value}
+        {@html currentIsland.value.nameHtml}
+      {:else}
+        <b> Select a marker to explore island...</b>
+      {/if}
+    </AppBar.Headline>
+    <AppBar.Trail>
+      <Icon type="home" link="/" height="64" tip="Home"></Icon>
+      <Separator />
+    </AppBar.Trail>
+  </AppBar.Toolbar>
+</AppBar>
