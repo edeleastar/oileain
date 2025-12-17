f9b041e7c66739f5514d876df59ef889541b0f85
Eamonn de Leastar <edeleastar@wit.ie>
2025-12-17 12:59:50 +0000

    Mainapp bar updates

--- src/lib/ui/components/Icon.svelte
+++ src/lib/ui/components/Icon.svelte
@@ -2,6 +2,7 @@
   import Icon from "@iconify/svelte";
   import { Portal, Tooltip } from "@skeletonlabs/skeleton-svelte";
   import { getIconColour, getIconType } from "../themes/icons";
+  import { currentView } from "$lib/runes.svelte";
 
   interface Props {
     type?: string;
@@ -13,6 +14,7 @@
     height?: string;
     tip?: string;
     text?: string;
+    view?: string;
   }
 
   let {
@@ -24,19 +26,23 @@
     width = "",
     height = "20",
     tip = $bindable(""),
-    text = ""
+    text = "",
+    view = ""
   }: Props = $props();
 
   // svelte-ignore state_referenced_locally
   if (target === "_blank") {
     tip = `${tip} (opens in a new Window)`;
   }
+
+  // Check if the current view matches the view prop using Svelte 5 runes
+  const isActive = $derived(view ? currentView.value === view : false);
 </script>
 
 {#snippet displayIcon()}
   {#if type}
     {#if link}
-      <div class="flex items-center rounded-lg p-2 hover:preset-tonal-secondary dark:hover:preset-tonal-tertiary">
+      <div class="flex items-center rounded-lg p-2 hover:preset-tonal-secondary dark:hover:preset-tonal-tertiary {isActive ? 'border-2 border-primary-500' : ''}">
         <a class="btn btn-sm" {target} href={link}>
           <Icon icon={getIconType(type)} color={getIconColour(type)} {width} {height} />
           {text}
@@ -46,7 +52,7 @@
       <Icon icon={getIconType(type)} color={getIconColour(type)} {width} {height} />
     {/if}
   {:else if link}
-    <a {target} href={link}>
+    <a {target} href={link} class={isActive ? 'border-2 border-primary-500 rounded-lg p-1' : ''}>
       {color}
       <Icon {icon} {color} {width} {height} />
     </a>
